5fee89701250a3748ebd65c28da6cda03816589e
rudyknowak <rudyknowak@gmail.com>
2018-03-04 14:02:08 +0100

    poprawka 1

--- src/MetricConverter.java
+++ src/MetricConverter.java
@@ -1,13 +1,32 @@
 public class MetricConverter {
 
-    int calculateCM(int wartosc1){
+    int calculateCMfromM(int wartosc1){
         int cm=wartosc1*100;
         return cm;
     }
 
-    int calculateMM(int wartosc1) {
+    int calculateMMfromM(int wartosc1) {
         int mm = wartosc1 * 1000;
         return mm;
     }
 
+    int calculateMfromCM(int wartosc1){
+        int m=wartosc1/100;
+        return m;
+    }
+
+    int calculateMMfromCM(int wartosc1){
+        int mm=wartosc1*10;
+        return mm;
+    }
+
+    int calculateMfromMM (int wartosc1){
+        int m=wartosc1/1000;
+        return m;
+    }
+
+    int calculateCMfromMM(int wartosc1){
+        int cm=wartosc1/10;
+        return cm;
+    }
 }
--- src/PrzelicznikServlet.java
+++ src/PrzelicznikServlet.java
@@ -11,25 +11,35 @@ public class PrzelicznikServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         request.setCharacterEncoding("UTF-8");
-        String metry = request.getParameter("metry");
-//      String centymetry = request.getParameter("centymetry");
-//      String milimetry = request.getParameter("milimetry");
-
-        Integer metryINT = Integer.valueOf(metry);
-
-//        Integer centymetryINT = Integer.valueOf(centymetry);
-//        Integer milimetryINT = Integer.valueOf(milimetry);
-
-        MetricConverter metConv = new MetricConverter();
-        int wynik1 = metConv.calculateCM(metryINT);
-
-
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html");
 
+        String metry = request.getParameter("metry"); //przypisuje stringa z formularza do stringa metry
+        String centymetry = request.getParameter("centymetry");
+        String milimetry = request.getParameter("milimetry");
+        MetricConverter metConv = new MetricConverter();
         PrintWriter writer = response.getWriter();
-        writer.print("Podana wartość w przeliczeniu na: \n centymetry: "+wynik1);
+
+
+        if (metry != null&& centymetry == null && milimetry == null ) { //sprawdzam czy metry nie jest nullem
+            Integer metryINT = Integer.valueOf(metry); //przerabiam metry na liczbę
+            int wynik1 = metConv.calculateCMfromM(metryINT);
+            int wynik2 = metConv.calculateMMfromM(metryINT);
+            writer.print("Podana wartość w przeliczeniu na:<br>metry: " + metryINT + "<br>centymetry: " + wynik1 + "<br>milimetry: " + wynik2);
+        } else if (metry == null && centymetry != null && milimetry == null) {
+            Integer centINT = Integer.valueOf(centymetry);
+            int wynik3 = metConv.calculateMfromCM(centINT);
+            int wynik4 = metConv.calculateMMfromCM(centINT);
+            writer.print("Podana wartość w przeliczeniu na:<br>metry: " + wynik3 + "<br>centymetry: " + centINT + "<br>milimetry: " + wynik4);
+        } else if (metry == null && centymetry == null && milimetry != null) {
+            Integer mmINT = Integer.valueOf(milimetry);
+            int wynik5 = metConv.calculateMfromMM(mmINT);
+            int wynik6 = metConv.calculateMfromCM(mmINT);
+            writer.print("Podana wartość w przeliczeniu na:<br>metry: " + wynik5 + "<br>centymetry: " + wynik6 + "<br>milimetry: " + mmINT);
+
+        }
     }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
 }
